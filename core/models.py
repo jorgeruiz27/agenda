16400cd6ea906624faeca6260279ec9d3c16bb2c
jorgeruiz27 <jorge.cx.27@gamil.com>
2022-06-02 22:41:40 -0400

    adcionado comandos de alteracao e exclusao de dados

--- core/models.py
+++ core/models.py
@@ -6,6 +6,7 @@ class Evento(models.Model):
     titulo = models.CharField(max_length=100, verbose_name='Título')
     descricao = models.TextField(blank=True, null=True)
     data_evento = models.DateTimeField(verbose_name='Data do Evento')
+    local_evento = models.TextField(blank=True, null=True)
     data_criacao = models.DateTimeField(auto_now=True, verbose_name='Data de Criação')
     usuario = models.ForeignKey(User, on_delete=models.CASCADE) #CASCADE se o usario for excluido será excluido todos o eventos deste usuario
 
@@ -17,3 +18,6 @@ class Evento(models.Model):
 
     def get_data_evento(self):
         return self.data_evento.strftime('%d/%m/%Y às %H:%M')
+
+    def get_data_input_evento(self):
+        return self.data_evento.strftime('%Y-%m-%dT%H:%M')
